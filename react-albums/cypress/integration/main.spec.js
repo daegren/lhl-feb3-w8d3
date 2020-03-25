5c4eba010658be590bbad91a87461e779599f76b
David Mills <dave@1morepx.com>
2020-03-25 11:52:38 -0400

    using fixtures and mocking routes

--- react-albums/cypress/integration/main.spec.js
+++ react-albums/cypress/integration/main.spec.js
@@ -19,44 +19,62 @@ context('Album Search', () => {
 
     cy.get('.filters').as('filters')
 
-    const assertBoxIsChecked = (filters, filter) => {
+    const assertBox = (filters, filter, checked = true) => {
+      const assertion = checked ? 'be.checked' : 'not.be.checked'
+
       cy.get(filters)
         .contains(filter)
         .parent()
         .find('input[type="checkbox"]')
-        .should('be.checked')
+        .should(assertion)
     }
 
-    const assertBoxIsNotChecked = (filters, filter) => {
-      cy.get(filters)
-        .contains(filter)
-        .parent()
-        .find('input[type="checkbox"]')
-        .should('not.be.checked')
-    }
-
-    assertBoxIsChecked('@filters', 'Explicit')
-    assertBoxIsChecked('@filters', '1900s')
-    assertBoxIsChecked('@filters', '2000s')
-    assertBoxIsNotChecked('@filters', 'Single')
-    assertBoxIsNotChecked('@filters', 'EP')
+    assertBox('@filters', 'Explicit')
+    assertBox('@filters', '1900s')
+    assertBox('@filters', '2000s')
+    assertBox('@filters', 'Single', false)
+    assertBox('@filters', 'EP', false)
   })
 
   it('fetches artists', () => {
+    cy.server()
+    cy.fixture('itunes.json').as('itunesResponse')
+    cy.route({
+      method: 'GET',
+      url: 'search*',
+      delay: 250,
+      response: '@itunesResponse'
+    }).as('getSearch')
+
     cy.get('.search')
       .find('input')
       .as('searchBox')
 
-    cy.get('@searchBox').type('Daft Punk')
+    cy.get('@searchBox')
+      .type('Daft Punk')
+      .should('have.value', 'Daft Punk')
+      .get('.spinner')
+      .as('spinner')
+      .should('be.visible')
 
-    cy.get('.album').should('have.length', 13)
+    cy.wait('@getSearch')
+      .get('main')
+      .find('.album')
+      .should('have.length', 13)
+
+    cy.get('@spinner').should('not.be.visible')
+
     cy.get('.album')
       .find('.album__info--explicit')
       .should('have.length', 1)
 
     cy.get('#Explicit').uncheck()
 
-    cy.get('.album').should('have.length', 12)
+    cy.wait(200)
+      .get('main')
+      .find('.album')
+      .should('have.length', 12)
+
     cy.get('.album')
       .find('.album__info--explicit')
       .should('not.exist')
